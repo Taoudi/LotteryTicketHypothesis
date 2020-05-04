aa46ffd5ee0e414a68efd9881f2288a0070475cb
Taoudi <Taoudi128@hotmail.com>
2020-05-04 14:55:41 +0200

    Update train_cnn.py

--- src/train_cnn.py
+++ src/train_cnn.py
@@ -12,17 +12,27 @@ class Simple_CNN:
     def __init__(self):
         self.model = models.Sequential()
         self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.1))
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.1))
         self.model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)))
         self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+        self.model.add(layers.Dropout(0.15))
         self.model.add(layers.BatchNormalization())
         self.model.add(layers.Conv2D(128, (3, 3), activation='relu'))
         self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Dropout(0.2))
         self.model.add(layers.BatchNormalization())
         self.model.add(layers.Conv2D(256, (3, 3), activation='relu'))
         self.model.add(layers.MaxPooling2D((2, 2)))
         self.model.add(layers.Flatten())
         #self.model.add(layers.BatchNormalization())
-        self.model.add(layers.Dense(512, activation='relu'))
+        self.model.add(layers.Dense(256, activation='relu'))
+        self.model.add(layers.Dense(256, activation='relu'))
         self.model.add(layers.Dense(10))
         #self.model.add(layers.Softmax())
         self.model.compile(optimizer='adam',
@@ -33,7 +43,7 @@ class Simple_CNN:
         self.bs = 128
 
     def fit_(self,X,Y):
-        history = self.model.fit(X, Y, epochs=self.epochs, validation_split=0.1,callbacks=[self.es],batch_size=self.bs) ,
+        history = self.model.fit(X, Y, epochs=self.epochs, validation_split=0.1,callbacks=[self.es],batch_size=self.bs, workers=1, use_multiprocessing=False) ,
         return history
 
     def eval_(self, testX, testY):
@@ -48,7 +58,8 @@ cnn = Simple_CNN()
 cnn.fit_(train_images, train_labels)
 loss, acc = cnn.eval_(test_images,test_labels)
 print(acc)
-
+if test_acc > 0.76:
+        cnn.save('models/best_net.h5')
 
     #X = train_images.reshape(np.size(train_images,0),3072)
     #testX = test_images.reshape(np.size(test_images,0),3072)
