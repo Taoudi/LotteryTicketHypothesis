aeab9deac489fb886df7469992a7d5d729998862
taoudi <taoudi128@gmail.com>
2020-05-08 23:26:50 +0000

    BIG TEST

--- src/plots.py
+++ src/plots.py
@@ -3,50 +3,52 @@ import matplotlib.pyplot as plt
 from constants import SETTINGS, PRUNING_PERCENTAGES
 
 def plot_lenet_mnist():
-    histories_reinit = np.load("data/histories_rand.npz", allow_pickle=True)['histories']
-    histories = np.load("data/iterpr_lenet_trainiter.npz", allow_pickle=True)['histories']
+    #histories_reinit = np.load("data/histories_rand.npz", allow_pickle=True)['histories']
+    histories = np.load("data/histories_50_iter.npz", allow_pickle=True)['histories']
     iterations = len(histories)-1
     iterations = 5
     S = [0.0, 1.0, 1.0, 0.5]
     c = 1-PRUNING_PERCENTAGES[1]**(1/iterations)
+    # percentages = PRUNING_PERCENTAGES
+    #markers = {
+    #    0: '8',
+    #    1: 'o',
+    #    2: 'v',
+    #    3: 'x',
+    #    4: 's',
+    #    5: '+',
+    #    6: '.'
+    #}
 
-    markers = {
-        0: '8',
-        1: 'o',
-        2: 'v',
-        3: 'x',
-        4: 's',
-        5: '+',
-        6: '.'
-    }
-
-    colors = {
-        0: 'b',
-        1: 'g',
-        2: 'r',
-        3: 'm',
-        4: 'k',
-        5: 'chartreuse',
-    }
-    percentages = np.zeros(iterations+1)
-    percentages[0] = 0.513
-    percentages[1] = 0.211
-    percentages[2] = 0.07
-    percentages[3] = 0.036
-    percentages[4] = 0.019
-    percentages[5] = 1.0
+    #colors = {
+    #    0: 'b',
+    #    1: 'g',
+    #    2: 'r',
+    #    3: 'm',
+    #    4: 'k',
+    #    5: 'chartreuse',
+    #}
+   # percentages = np.zeros(iterations+1)
+   # percentages[0] = 0.513
+   # percentages[1] = 0.211
+   # percentages[2] = 0.07
+   # percentages[3] = 0.036
+   # percentages[4] = 0.019
+   # percentages[5] = 1.0
     #percentages[iterations] = 1
-    #s = S[1]
-    #for i in range(0,iterations):
-        #s = np.around(s - s*c, decimals=3)
-        #percentages[i] = s
-
-    for i in range(2, iterations):
-        plt.plot(range(0,len(histories[i,:])*1000,1000), histories[i,:], label=percentages[i], marker=markers[i], color=colors[i])
+    s = S[1]
+    percentages = np.zeros(iterations)
+    for i in range(0,iterations):
+        s = np.around(s - s*c, decimals=3)
+        percentages[i] = s
+    print(histories.shape)
+    print(len(percentages))	
+    for i in range(0, iterations):
+        plt.plot(range(0,len(histories[i,:])*1000,1000), histories[i,:], label=percentages[i])
     
 
-    for i in range(2, iterations):
-        plt.plot(range(0,len(histories_reinit[i,:])*1000,1000), histories_reinit[i,:], label=percentages[i], marker=markers[i], color=colors[i], linestyle='--')
+    #for i in range(2, iterations):
+    #    plt.plot(range(0,len(histories_reinit[i,:])*1000,1000), histories_reinit[i,:], label=percentages[i], marker=markers[i], color=colors[i], linestyle='--')
     #plt.plot(range(0,len(histories_reinit[0,:])*1000,1000), histories_reinit[0,:], 
     #label=str(percentages[0]) + "(reinit)", color=colors[0], marker='8', linestyle='--')
     
