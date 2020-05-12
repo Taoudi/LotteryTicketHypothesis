65d0557042489096fef639906c11e1d079464438
Taoudi <Taoudi128@hotmail.com>
2020-05-12 19:45:04 +0200

    models set up

--- src/conv_cifar/conv_experiment.py
+++ src/conv_cifar/conv_experiment.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from constants import CIFAR10_DATA, SETTINGS_CONV2, SETTINGS_CONV4
-from conv_models import CONV2_NETWORK, CONV4_NETWORK
+from constants import CIFAR10_DATA, SETTINGS_CONV2, SETTINGS_CONV4,SETTINGS_CONV6
+from conv_models import CONV2_NETWORK, CONV4_NETWORK,CONV6_NETWORK
 from tools import generate_percentages
 from pruning import oneshot_pruning
 from tensorflow.keras import layers
@@ -27,8 +27,24 @@ def simple_test4():
     #network.model.summary()
     print(test_acc)
 
+def iterative_test2():
+    percents,iterations = generate_percentages([1.0,1.0,1.0],0.02)
+    iterations = 4
+    og_network = CONV2_NETWORK()
+    init_weights = og_network.get_weights()
+    og_network.fit(x_train, y_train, SETTINGS_CONV2)
+    test_loss, test_acc = og_network.evaluate_model(x_test,y_test)
+    for i in range(0,iterations,2):
+        print("Dense %: " + str(percents[i][1]))
+        print("Conv/Output %: " + str(percents[i][0]))
+        mask = oneshot_pruning(og_network, percents[i][0],percents[i][1],percents[i][0])
+        pruned_network = SETTINGS_CONV2()
+        pruned_network.fit_batch(x_train,y_train,mask,init_weights,SETTINGS_CONV2,x_test,y_test)
+        pruned_network.evaluate_model(x_test,y_test)
 
-def simple_oneshot_test4():
+        og_network = pruned_network
+
+def iterative_test4():
     percents,iterations = generate_percentages([1.0,1.0,1.0],0.02)
     iterations = 4
     og_network = CONV4_NETWORK()
@@ -45,10 +61,26 @@ def simple_oneshot_test4():
 
         og_network = pruned_network
         
+        
+def iterative_test6():
+    percents,iterations = generate_percentages([1.0,1.0,1.0],0.02)
+    iterations = 4
+    og_network = CONV6_NETWORK(SETTINGS_CONV6['early_stopping'])
+    init_weights = og_network.get_weights()
+    og_network.fit(x_train, y_train, SETTINGS_CONV6)
+    test_loss, test_acc = og_network.evaluate_model(x_test,y_test)
+    for i in range(0,iterations,2):
+        print("Dense %: " + str(percents[i][1]))
+        print("Conv/Output %: " + str(percents[i][0]))
+        mask = oneshot_pruning(og_network, percents[i][0],percents[i][1],percents[i][0])
+        pruned_network = CONV6_NETWORK()
+        pruned_network.fit_batch(x_train,y_train,mask,init_weights,SETTINGS_CONV6,x_test,y_test)
+        pruned_network.evaluate_model(x_test,y_test)
 
+        og_network = pruned_network
 
     #_,epoch = og_network.fit_batch(x_train, y_train, mask, init_weights, SETTINGS_CONV2, x_test, y_test)
     #test_loss,test_acc = og_network.evaluate_model(x_test, y_test)
     #network.model.summary()
 if __name__ == "__main__":
-    simple_oneshot_test4()
+    iterative_test6()
