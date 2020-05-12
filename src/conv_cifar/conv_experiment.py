5ce167bc5be57e0a438d9b431adac20a3acdd3d0
Taoudi <Taoudi128@hotmail.com>
2020-05-12 04:29:51 +0200

    Update conv_experiment.py

--- src/conv_cifar/conv_experiment.py
+++ src/conv_cifar/conv_experiment.py
@@ -3,6 +3,8 @@ from constants import CIFAR10_DATA, SETTINGS_CONV2, SETTINGS_CONV4
 from conv_models import CONV2_NETWORK, CONV4_NETWORK
 from tools import generate_percentages, get_weights
 from pruning import oneshot_pruning
+from tensorflow.keras import layers
+
 (x_train, y_train), (x_test, y_test) = CIFAR10_DATA.load_data()
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -29,17 +31,17 @@ def simple_test4():
 def simple_oneshot_test4():
     percents,iterations = generate_percentages([1.0,1.0,1.0],0.02)
     percents = percents[14]
+    print(percents)
     og_network = CONV4_NETWORK()
     init_weights = get_weights(og_network)
-    mask = oneshot_pruning(og_network, percents[0],percents[1],percents[2])
-    print("WTF")
-    for i in mask:
-        print(i.shape)
-    #og_network.fit(x_train, y_train, SETTINGS_CONV4)
-    #test_loss, test_acc = og_network.evaluate_model(x_test,y_test)
+    og_network.fit(x_train, y_train, SETTINGS_CONV4)
+    test_loss, test_acc = og_network.evaluate_model(x_test,y_test)
     
     mask = oneshot_pruning(og_network, percents[0],percents[1],percents[2])
-    #print(mask)
+
+    for i,m in enumerate(mask):
+        if isinstance(og_network.model.layers[i],layers.Conv2D):
+            print(m)
 
     
 
